f768da6574d54738557a610ae04bc983c9274989
Carlos314159 <84247106+CarlosDev314159@users.noreply.github.com>
2022-03-27 22:39:44 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import os, re, webbrowser, time
+from colorama import Fore, init, Back
+# importing libs
+
+
+def commands():
+    terminal_commands = ["sudo apt update",
+                    "sudo apt install build-essential git dkms",
+                    "git clone https://github.com/brektrou/rtl8821CU.git",
+                    "cd rtl8821CU",
+                    "chmod +x dkms-install.sh",
+                    "sudo ./dkms-install.sh",
+                    "sudo modprobe 8821cu]"
+                    ]
+    teste = ["sudo apt-get update", "sudo apt full-upgrade"]
+    os.chdir("/home")
+    for command in teste:
+        os.system(command)
+
+# function for execute to the commmands
+
+init(autoreset=True)
+print("D-Link DWA 171 WIFI ADAPTER AUTOMATIC DRIVER INSTALLER")
+print(Fore.RED + "Works only on Debian distros")
+time.sleep(3)
+
+# finding some dependencies
+
+item = os.popen("apt list").read()
+find = re.search("git-all", item)
+print("Checking for dependencies...")
+
+if find:
+    print(Fore.GREEN + "OK")
+    print("No dependency found\n")
+    time.sleep(1)
+    commands()
+else:
+    # If any dependencies were found and not downloaded in the terminal, you will be directed to the website of the missing package
+    print(Fore.RED + "Dependency found!")
+    print("We'll download it for you!\n")
+    time.sleep(1)
+    download_git = os.system("sudo apt install git -y")
+    
+    if download_git == 25600:
+        print("Package not found, you will be directed to the website...")
+        webbrowser.open("https://git-scm.com/downloads")
+        print(Fore.YELLOW + "Please restart the script")
+        exit()
+    else:  
+        print(Fore.GREEN + "Drivers installed successfully")
